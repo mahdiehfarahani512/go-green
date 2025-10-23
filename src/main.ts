158979c4a4418fb1be3d867127b925fd642f5c4d
mahdieh <mahdiefarahani00@gmail.com>
2025-10-23 16:47:18 +0330

    v4

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
 import "./style.css";
+const nextbtn=document.getElementById("nxtbtn")as HTMLButtonElement
+const privbtn=document.getElementById("pribtn")as HTMLButtonElement
+const sliderEl=document.querySelectorAll(".slide")
+
+
+nextbtn?.addEventListener("click",()=>{
+    const currentEl=document.querySelector(".current")
+    currentEl?.classList.remove("current")
+    if(currentEl?.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("current")
+    }else{
+        sliderEl[0].classList.add("current")
+    }
+})
+privbtn?.addEventListener("click",()=>{
+    const currentEl=document.querySelector(".current")
+    currentEl?.classList.remove("current")
+    if(currentEl?.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("current")
+    }else{
+        sliderEl[0].classList.add("current")
+    }
+})
+setInterval(()=>{
+       const currentEl=document.querySelector(".current")
+        currentEl?.classList.remove("current")
+        if(currentEl?.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("current")
+    }else{
+        sliderEl[0].classList.add("current")
+    }
+
+},5000)
