84399cf293db46ce15777679005a701f4c092f40
mahdieh <mahdiefarahani00@gmail.com>
2025-10-24 22:46:39 +0330

    v5

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import "./style.css";
 const nextbtn=document.getElementById("nxtbtn")as HTMLButtonElement
 const privbtn=document.getElementById("pribtn")as HTMLButtonElement
 const sliderEl=document.querySelectorAll(".slide")
-
+const btntotopEl=document.getElementById("totop")as HTMLDivElement
+const divtobottom=document.getElementById("tobottom")as HTMLDivElement
+const htmlEl=document.getElementById("html")as HTMLDivElement
+const cssEl=document.getElementById("css")as HTMLDivElement
+const jqueryEl=document.getElementById("jquery")as HTMLDivElement
+const phpEl=document.getElementById("php")as HTMLDivElement
 
 nextbtn?.addEventListener("click",()=>{
     const currentEl=document.querySelector(".current")
@@ -32,3 +37,35 @@ setInterval(()=>{
     }
 
 },5000)
+
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset>=200){
+        btntotopEl.classList.remove("toup")
+    }else{
+        btntotopEl.classList.add("toup")
+    }
+})
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset>=250){
+        divtobottom.classList.add("tobott")
+    }else{
+        divtobottom.classList.remove("tobott")
+    }
+})
+
+if(window.pageYOffset>=3345){
+htmlEl.classList.add("animation1")
+cssEl.classList.add("animation2")
+jqueryEl.classList.add("animation3")
+phpEl.classList.add("animation4")
+}
+
+// const startAdd1=setInterval(() => {
+//     counter1El.textContent=count1.toString()
+//     if(count1>=100){
+//     clearInterval(startAdd1)
+//   }else{
+//   count1++
+// }
+// }, 330);
+
